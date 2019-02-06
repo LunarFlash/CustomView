bdfac2bf2d5707cb873066b34fa39696355895ad
Terry Wang <logins.yiwang@gmail.com>
2019-02-06 12:27:32 -0500

    Initial Commit

---
+++ Layout Driven UI/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Layout Driven UI
+//
+//  Created by Yi Wang on 2/6/19.
+//  Copyright © 2019 Accenture. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
