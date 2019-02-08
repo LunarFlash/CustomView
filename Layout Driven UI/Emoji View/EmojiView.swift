872170c814b25040952655da728ccca54f7c090d
Terry Wang <terry.wang@accenture.com>
2019-02-07 22:24:16 -0500

    comment

--- Layout Driven UI/Emoji View/EmojiView.swift
+++ Layout Driven UI/Emoji View/EmojiView.swift
@@ -34,7 +34,7 @@ class EmojiView: UIView, Xibable {
     var mood: Mood = Mood.groovy {
         // didSet is a property observer that triggers when this variable changes
         didSet {
-            // Dirty the layout, triggers layoutSubViews at the next drawing cycle.
+            // Dirty the layout, triggers layoutSubViews at the next draw cycle.
             setNeedsLayout()
         }
     }
